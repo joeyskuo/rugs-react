7e87c05e37696ce8ee2a13988862392bf9e92a9b
Joey <joeyskuo@gmail.com>
2022-09-03 10:51:29 -0700

    added cart title, items, checkout button component stylings

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,14 +4,25 @@ import "../css/_cart.css";
 const Cart = ({cartItems}) => {
     return (
         <div id="cart">
+            <div className="cart-backdrop" onClick={() => {console.log("clicked")}}>
+            </div>
             <div className="cart-summary">
-            <span>Cart Summary</span>
-            {cartItems.map((product) => (
-                <ProductCard
-                    key={product.parentProductId + "-cart"}
-                    product={product}
-                />
-            ))}
+                <div>
+                    <span>Cart Summary</span>
+                    <span>X</span>
+                </div>
+                <div className="cart-summary--items">
+                    {cartItems.map((product) => (
+                        <ProductCard
+                            key={product.parentProductId + "-cart"}
+                            product={product}
+                        />
+                    ))}
+                </div>
+                <div>
+                    <p>Subtotal (3 items):  $9600.00</p>
+                    <button>View Cart & Checkout</button>
+                </div>
             </div>
         </div>
     )
